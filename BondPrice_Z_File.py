b69f84d651c730bee64018cfea9ad6cad30df3b5
hl3835-lhl <hl3835@columbia.edu>
2025-10-05 00:45:54 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,15 @@
-
-
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    price = 0.0
+    c = face * couponRate
+    m = len(times)
+    for i in range(m):
+        if i == m - 1:
+            cf = c + face
+        else:
+            cf = c
+        t = times[i]
+        y = yc[i]
+        pvcf = cf * (1 + y) ** (-t)
+        price = price + pvcf
+    getBondPrice_Z = price
+    return getBondPrice_Z
