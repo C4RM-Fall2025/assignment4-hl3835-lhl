66c05835f1a09a6345d7b906782fd144e3fca0ce
hl3835-lhl <hl3835@columbia.edu>
2025-10-05 00:28:24 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,18 +1,14 @@
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    n = int(m * ppy)
-    c = face * couponRate / ppy
-    r = y / ppy
-    pvSum = 0.0
-    wSum = 0.0
-    for i in range(1, n + 1):
-        if i == n:
-            cf = c + face
+    sum = 0.0
+    cf = face * couponRate / ppy
+    for i in range(1, m * ppy + 1):
+        if i == m * ppy:
+            pvcf = (cf + face) * (1 + y / ppy) ** (-i)
         else:
-            cf = c
-        pvcf = cf * (1 + r) ** (-i)
-        pvSum = pvSum + pvcf
-        wSum = wSum + pvcf * i
-    getBondDuration = wSum / pvSum
+            pvcf = cf * (1 + y / ppy) ** (-i)
+        sum = sum + pvcf
+    getBondPrice = sum
     return getBondDuration
+
 
