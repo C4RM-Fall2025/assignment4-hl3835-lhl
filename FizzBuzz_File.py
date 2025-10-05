b895ba37b33262520d6f01ba1f6ac42e60d5ac68
hl3835-lhl <hl3835@columbia.edu>
2025-10-05 00:53:01 -0400

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,18 @@
 
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 424, 'fizzbuzz']
-    return(v)
+    m = finish - start + 1
+    fb = []
+    for i in range(m):
+        num = start + i
+        if (num % 3 == 0) and (num % 5 == 0):
+            fb.append("fizzbuzz")
+        elif num % 3 == 0:
+            fb.append("fizz")
+        elif num % 5 == 0:
+            fb.append("buzz")
+        else:
+            fb.append(num)
+    FizzBuzz = fb
+    return FizzBuzz
+
